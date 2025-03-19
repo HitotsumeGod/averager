cf7441400c3e8dd32d0d9b94c961830f1d2e968f
peternaylor <peter@oldtiogafarm.com>
2025-03-19 07:20:47 -0400

    commit for push

--- src/main/averager.c
+++ src/main/averager.c
@@ -6,31 +6,49 @@
 int main(int argc, char *argv[]) {
 
 	FILE *fptr;
-	unsigned int res, count;
+	long fsize;
+	int *numarr, count;
 	char *buf;
 	if (argc != 2) {
 		printf("%s\n", "Improper format. Please launch the applicaton with a single argument (the filename of the file to be read from). Example : \"averager.exe myfile.txt\".");
-		exit(EXIT_FAILURE);
-	}
-	if ((buf = malloc(sizeof(char) * BUFLEN)) == NULL) {
-		perror("malloc err");
 		exit(EXIT_FAILURE);
 	}
 	if ((fptr = fopen(argv[1], "r")) == NULL) {
 		printf("%s\n", "File not found. Please provide the file in the same directory as this program and then provide the filename.");
 		exit(EXIT_FAILURE);
 	}
-	res = 0, count = 0;
+	if (fseek(fptr, 0, SEEK_END) == -1) {
+		perror("fseek err");
+		exit(EXIT_FAILURE);
+	}
+	if ((fsize = ftell(fptr)) == -1) {
+		perror("ftell err");
+		exit(EXIT_FAILURE);
+	}
+	rewind(fptr);
+	if ((buf = malloc(sizeof(char) * BUFLEN)) == NULL || (numarr = malloc(sizeof(int) * fsize)) == NULL) {
+	       perror("malloc err");
+		exit(EXIT_FAILURE);
+	}		
+	count = 0;
 	while (fgets(buf, BUFLEN, fptr) != NULL) {
-		res += atoi(buf);
+		//printf("%d\t", atoi(buf));
+		*(numarr + count) = atoi(buf);
 		count++;
 	}
-	printf("%s%u%c\n", "Numerical average is ", res / count, '.');
+	unsigned long res = 0;
+	int i;
+	for (i = 0; i < fsize; i++) {
+		res += *(numarr + i);
+	}
+	printf("king%d\n", i);
+	printf("%lu\n", res / fsize);
 	if (fclose(fptr) == -1) {
 		perror("fclose err");
 		exit(EXIT_FAILURE);
 	}
 	free(buf);
+	free(numarr);
 	return 0;
 
 }
